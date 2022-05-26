514e7e5462fc6b4d7a944edabd1ac30159d5a471
TranDinhViet01 <101802820+TranDinhViet01@users.noreply.github.com>
2022-05-26 15:17:23 +0700

    sua con

--- DangNhap.sql
+++ DangNhap.sql
@@ -10,31 +10,22 @@ create table NhanVien(
 	tenNV varchar(50) not null,
 	ngaysinh date not null,
 	gioitinh varchar(10) not null,
-	chucVu varchar(10) not null,
+	loaiNV varchar(10) not null,
 	SDT varchar(11) not null,
-	tenTK varchar(50) not null
+	matkhauNV varchar(50) not null
 )
 go
---Tạo table DiaChi
-create table DiaChi(
-	maNV varchar(10) not null primary key,
-	XaPhuong varchar(30) not null,
-	QuanHuyen varchar(30) null,
-	TinhTP varchar(30) null,
-)
-go
---Tạo table TaiKhoan
-create table TaiKhoan(
-	tenTK varchar(50) not null primary key,
-	matKhau varchar(30) not null
-)
+
 go
 --Tạo table QuanLyXe
 create table QuanLyXe(
 	bienSoXe varchar(15) not null primary key,
+	maVe varchar(10) not null,
 	loaiXe varchar(30) not null,
 	tenXe varchar(30) not null,
-	mauXe varchar(30) not null
+	mauXe varchar(30) not null,
+	ngayVaoBai date not null,
+	ngayRaBai date null
 )
 go
 --Tạo table QuanLyVe
@@ -43,15 +34,6 @@ create table QuanLyVe(
 	loaiVe varchar(30) not null,
 	giaVe money not null,
 	trangThaiVe varchar(20) not null
-)
-go
---Tạo table QuanLyBaiXe
-create table QuanLyBaiXe(
-	maBanGhi varchar(10) not null primary key,
-	bienSoXe varchar(15) not null,
-	maVe varchar(10) not null,
-	ngayVaoBai date not null,
-	ngayRaBai date null
 )
 go
 --Tạo table TroGiup
@@ -63,46 +45,23 @@ create table TroGiup(
 	noiDung varchar(100) not null,
 )
 go
---2/Thêm Foreign Key
---Thêm Foreỉgn key cho bảng DiaChi
-alter table DiaChi
-add constraint FK_maNV_DiaChi foreign key(maNV) references NhanVien(maNV)
-go
---Thêm Foreỉgn key cho bảng NhanVien
-alter table NhanVien
-add constraint FK_tenTK_NhanVien foreign key(tenTK) references TaiKhoan(tenTK)
-go
---Thêm Foreỉgn key cho bảng QuanLyBaiXe
-alter table QuanLyBaiXe
-add constraint FK_bienSoXe_QuanLyBaiXe foreign key(bienSoXe) references QuanLyXe(bienSoXe),
-	constraint FK_maVe_QuanLyBaiXe foreign key(maVe) references QuanLyVe(maVe)
-go
---3/Chèn dữ liệu
---Chèn dữ liệu vào bảng TaiKhoan
-insert into TaiKhoan
-values ('Phi123','khoa4431'),
-	   ('Nhi334','khoa4431'),
-	   ('Dung977','long3124')
-go
+--2/Chèn dữ liệu
 --Chèn dữ liệu vào bảng NhanVien
 set dateformat dmy
-insert into NhanVien(maNV,tenNV,ngaysinh,gioitinh,chucVu,SDT,tenTK)
+insert into NhanVien(maNV,tenNV,ngaysinh,gioitinh,loaiNV,SDT,matKhauNV)
 values ('205131','Hoang Thi Phi','2/10/2002','nu','Nhan Vien','0968845331','Phi123'),
 	   ('205123','Thai Thi Nhi','8/4/1998','nu','Nhan Vien','0899431732','Nhi334'),
 	   ('205177','Tran Hung Dung','13/9/1995','nam','Quan Ly','0981445678','Dung977')
 go
---Chèn dữ liệu vào bảng QuanLyXe
-insert into QuanLyXe
-values ('K2309','Xe may','Honda','Den'),
-	   ('K2311','Xe oto','Toyota','Trang')
-go
+
 --Chèn dữ liệu vào bảng QuanLyVe
 insert into QuanLyVe
 values ('VN000001','Ve ngay',5000,'Tot'),
-	   ('VT000001','Ve thang',130000,'Tot'),
-	   ('VT000002','Ve thang',130000,'Bi hu')
+	   ('VT000002','Ve thang',130000,'Tot'),
+	   ('VT000003','Ve thang',130000,'Bi hu')
+
+--Chèn dữ liệu vào bảng QuanLyXe
 go
---Chèn dữ liệu vào bảng QuanLyBaiXe
-insert into QuanLyBaiXe
-values ('BG001','K2309','VN000001','10/3/2002','2022/5/4'),
-	   ('BG002','K2311','VT000001','11/9/2003',null)
+set dateformat dmy insert into QuanLyXe
+values ('K2309','VN000001','Xe may','Honda','Den','13/4/2022','14/4/2022'),
+	   ('K2311','VN000002','Xe oto','Toyota','Trang','13/4/2022','14/4/2022')
